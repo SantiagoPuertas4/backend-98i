8d7f4ab3d85425c3ab718e824f46ced83ea2e13a
Santiago Puertas <santidoors13@gmail.com>
2024-08-15 18:40:05 -0300

    Agrego validaciones post y put

--- src/routes/routers/blogRouter.js
+++ src/routes/routers/blogRouter.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import { Blogs } from '../../controllers/blogs/index.js';
+import { validateBody } from '../../middlewares/validateBody.js';
+import { post_put_blogValidationSchema } from '../../helpers/validationSchemas/blogValidationSchemas.js';
 
 export const blogRouter = express.Router();
 blogRouter.get('/', Blogs.GetController.getBlogs);
 
-blogRouter.post('/', Blogs.PostController.postBlog);
+blogRouter.post(
+  '/',
+  (req, res, next) =>
+    validateBody(req, res, next, post_put_blogValidationSchema),
+  Blogs.PostController.postBlog,
+);
 
-blogRouter.put('/:id', Blogs.PutController.putBlog);
+blogRouter.put(
+  '/',
+  (req, res, next) =>
+    validateBody(req, res, next, post_put_blogValidationSchema),
+  Blogs.PutController.putBlog,
+);
 
 blogRouter.delete('/:id', Blogs.DeleteController.deleteBlog);
