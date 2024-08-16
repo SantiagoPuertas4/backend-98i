1f2346b4933434e66327b122f72c9d27cdacb39b
Santiago Puertas <santidoors13@gmail.com>
2024-08-16 00:14:51 -0300

    Agrego post de login

---
+++ src/controllers/auth/controllers/PostController.js
@@ -0,0 +1,47 @@
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import HttpCodes from 'http-status-codes';
+import { internalError } from '../../../helpers/helpers.js';
+import UserModel from '../../../models/userSchema.js';
+
+export class PostController {
+  static async postLogin(req, res) {
+    const { body } = req;
+    try {
+      const user = await UserModel.findOne({
+        username: body.username,
+        isActive: true,
+      });
+
+      if (!user || !bcryptjs.compareSync(body.password, user.password)) {
+        res.status(HttpCodes.UNAUTHORIZED).json({
+          data: null,
+          message: 'Usuario y/o contraseña incorrectos',
+        });
+        return;
+      }
+
+      const userInfo = {
+        user: {
+          firstname: user.firstname,
+          lastname: user.lastname,
+          username: user.username,
+          isAdmin: user.isAdmin,
+        },
+      };
+
+      const token = jwt.sign(userInfo, process.env.SECRET_KEY, {
+        expiresIn: '1h',
+      });
+
+      console.log(user);
+
+      res.json({
+        data: token,
+        message: 'Logueo exitoso',
+      });
+    } catch (e) {
+      internalError(res, e, 'Ocurrio un erro intentando iniciar sesion');
+    }
+  }
+}
